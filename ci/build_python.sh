df4c27d6db308fa2edccaa2bfbf94ed9cac52eee
jakirkham <jakirkham@gmail.com>
2025-02-27 01:01:01 -0800

    Replace `conda-mambabuild`with `conda-build` (#16)
    These are now equivalent in behavior. However support for
    `conda-mambabuild` is being dropped. So switch to `conda-build`.

--- ci/build_python.sh
+++ ci/build_python.sh
@@ -13,7 +13,7 @@ sccache --zero-stats
 CMAKE_GENERATOR=Ninja \
 CONDA_OVERRIDE_CUDA="${RAPIDS_CUDA_VERSION}" \
 LEGATERAFT_PACKAGE_VERSION=$(head -1 ./VERSION) \
-rapids-conda-retry mambabuild \
+rapids-conda-retry build \
     --channel legate \
     --channel legate/label/rc \
     --channel legate/label/experimental \
